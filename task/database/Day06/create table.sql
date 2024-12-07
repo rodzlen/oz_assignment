142a9313b239d4e3af27b1c8c66658464b20a58f
rodzlen <qkqhrkxdkdy@naver.com>
2024-12-07 15:46:45 +0900

    📝 과제 업데이트

--- task/database/Day06/create table.sql
+++ task/database/Day06/create table.sql
@@ -65,38 +65,83 @@
 -- 	FOREIGN KEY (store_id) REFERENCES stores(id)
 -- );
 
+-- 
+-- CREATE TABLE order_records(
+-- 	id INT AUTO_INCREMENT PRIMARY KEY, -- 인덱싱
+-- 	employee_id INT, -- users 테이블의 id
+-- 	supplier_id INT, -- suppliers 테이블의 id
+-- 	change_date DATETIME DEFAULT CURRENT_TIMESTAMP, -- 변경된 시간
+-- 	raw_material_id INT, -- raw_material 테이블의 id
+-- 	quantity INT NOT NULL, -- 수량
+-- 	create_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- 생성된 시간
+-- 	FOREIGN KEY (employee_id) REFERENCES employees(id),
+-- 	FOREIGN KEY (raw_material_id) REFERENCES raw_materials(id),
+-- 	FOREIGN KEY (supplier_id) REFERENCES suppliers(id)
+-- );
+-- 
+-- CREATE TABLE sales_records(
+-- 	id INT AUTO_INCREMENT PRIMARY KEY, -- 인덱싱
+-- 	user_id INT, -- users 테이블의 id,
+-- 	store_id INT, -- stores 테이블의 id
+-- 	is_refund BOOL DEFAULT FALSE, -- 반품하는 것인지 확인하는 컬럼
+-- 	created_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- 생성된 시간
+-- 	FOREIGN KEY (user_id) REFERENCES users(id),
+-- 	FOREIGN KEY (store_id) REFERENCES stores(id)
+-- );
+-- 
+-- CREATE TABLE sales_items(
+-- 	id INT AUTO_INCREMENT PRIMARY KEY, -- 인덱싱
+-- 	sales_record_id INT, -- sales_records 테이블의 id
+-- 	product_id INT, -- products의 id
+-- 	quantity INT NOT NULL, -- 수량
+-- 	created_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- 생성된 시간
+-- 	FOREIGN KEY (sales_record_id) REFERENCES sales_records(id),
+-- 	FOREIGN KEY (product_id) REFERENCES products(id)
+-- );
+use db_test;
 
-CREATE TABLE order_records(
-	id INT AUTO_INCREMENT PRIMARY KEY, -- 인덱싱
-	employee_id INT, -- users 테이블의 id
-	supplier_id INT, -- suppliers 테이블의 id
-	change_date DATETIME DEFAULT CURRENT_TIMESTAMP, -- 변경된 시간
-	raw_material_id INT, -- raw_material 테이블의 id
-	quantity INT NOT NULL, -- 수량
-	create_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- 생성된 시간
-	FOREIGN KEY (employee_id) REFERENCES employees(id),
-	FOREIGN KEY (raw_material_id) REFERENCES raw_materials(id),
-	FOREIGN KEY (supplier_id) REFERENCES suppliers(id)
-);
+-- 1
+select * from users 
+	where  (first_name like '%s%' and first_name like '%y')
+	or (last_name like '%s%' and last_name like '%y');
+-- 2
+select u.contact from users u
+	join employees e on u.id = e.user_id
+	where e.store_id = 5;
+-- 3
+select * from suppliers s, stores st
+order by s.name,st.name;
 
-CREATE TABLE sales_records(
-	id INT AUTO_INCREMENT PRIMARY KEY, -- 인덱싱
-	user_id INT, -- users 테이블의 id,
-	store_id INT, -- stores 테이블의 id
-	is_refund BOOL DEFAULT FALSE, -- 반품하는 것인지 확인하는 컬럼
-	created_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- 생성된 시간
-	FOREIGN KEY (user_id) REFERENCES users(id),
-	FOREIGN KEY (store_id) REFERENCES stores(id)
-);
+-- 4
+select count(*) from employees
+where type = 'staff' or type = 'manager';
 
-CREATE TABLE sales_items(
-	id INT AUTO_INCREMENT PRIMARY KEY, -- 인덱싱
-	sales_record_id INT, -- sales_records 테이블의 id
-	product_id INT, -- products의 id
-	quantity INT NOT NULL, -- 수량
-	created_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- 생성된 시간
-	FOREIGN KEY (sales_record_id) REFERENCES sales_records(id),
-	FOREIGN KEY (product_id) REFERENCES products(id)
-);
+-- 5
+select u.first_name,u.last_name, s.name as '가게이름' from users u
+	join employees e on u.id = e.user_id
+	join stores s on s.id = e.store_id;
 
+-- 6
+select st.name, p.name, count(si.quantity) as '판매수량' from sales_items si
+	join sales_records sr on sr.id = si.sales_record_id
+	join products p on p.id = si.product_id
+	join stores st on sr.store_id = st.id
+	group by st.name, p.name;
+
+SELECT sr.store_id, p.name, s.quantity, COUNT(*) as popular
+FROM sales_items s
+JOIN products p ON s.product_id = p.id
+JOIN sales_records sr ON s.sales_record_id = sr.id
+GROUP BY sr.store_id, p.name, s.quantity;
+
+-- 7
+select r.name , count(DISTINCT s.id ) as '공급업체 수' from order_records o
+join raw_materials r on o.raw_material_id = r.id
+join suppliers s on o.supplier_id = s.id
+group by r.name;
+	
+SELECT type, COUNT(*) as employee_count
+FROM employees
+WHERE type IN ('staff', 'manager')
+GROUP BY type;
 
