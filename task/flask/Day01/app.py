fdf19266ac8641394e1993294fd5e32a4798c6b2
rodzlen <qkqhrkxdkdy@naver.com>
2025-01-03 18:36:37 +0900

    📝 챌린지 과제 구현 완료

--- task/flask/Day01/app.py
+++ task/flask/Day01/app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
-host = '127.0.0.1'
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    users = [
+    {"username": "traveler", "name": "Alex"},
+    {"username": "photographer", "name": "Sam"},
+    {"username": "gourmet", "name": "Chris"}
+    ]
+    return render_template('/index.html',users=users)
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
