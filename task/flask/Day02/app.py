e1e8a7bfa2b99c1a7b325ed5f134adb053f0401b
rodzlen <qkqhrkxdkdy@naver.com>
2025-01-06 17:09:22 +0900

    📝 Import api.py
    api.py의 book_blp 사용을 위한 import 및 해당 블루프린트 등록

--- task/flask/Day02/app.py
+++ task/flask/Day02/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_smorest import Api
+from api import book_blp
 
 app = Flask(__name__)
 
@@ -11,6 +12,7 @@ app.config['OPENAPI_SWAGGER_UI_PATH'] = "/swagger-ui-doc"
 app.config['OPENAPI_SWAGGER_UI_URL'] = "https://cdn.jsdelivr.net/npm/npm.swagger-ui-dist"
 
 api = Api(app)
+api.register_blueprint(book_blp)
 
 if __name__ == '__main__':
     app.run(debug=True)
