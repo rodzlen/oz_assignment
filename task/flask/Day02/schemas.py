31251b33607f456a383c9904fd18bffd62216105
rodzlen <qkqhrkxdkdy@naver.com>
2025-01-06 17:16:49 +0900

    주석추가

--- task/flask/Day02/schemas.py
+++ task/flask/Day02/schemas.py
@@ -1,6 +1,7 @@
 from marshmallow import Schema, fields
 
 class BookSchema(Schema):
-    id =fields.Int(dump_only=True)
+    
+    id =fields.Int(dump_only=True) # dump_only는 서버에서 직접 데이터를 관리
     title =fields.String(required=True)
     author =fields.String(required=True)
